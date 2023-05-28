bb02772820c618f78f412d7f5d7fbb57428c32ce
Matheus_S-Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2023-05-28 14:01:39 -0300

    Initial commit

---
+++ src/main/java/com/futuroDev/labReceitas/LabReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.futuroDev.labReceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futuroDev/labReceitas/LabReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuroDev.labReceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
